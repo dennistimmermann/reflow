118796d5e0dc20ca6214229855374add395e10dd
Dennis Timmermann <>
2026-04-26 22:18:21 +0200

    local editor running

--- ui/screens/home/home_gen.c
+++ ui/screens/home/home_gen.c
@@ -48,6 +48,8 @@ lv_obj_t * home_create(void)
     lv_obj_t * lv_label_0 = lv_label_create(lv_obj_0);
     lv_label_set_text(lv_label_0, "Hello world");
     lv_obj_set_style_text_font(lv_label_0, text_option, 0);
+    lv_obj_set_width(lv_label_0, 0);
+    lv_obj_set_height(lv_label_0, 0);
 
     LV_TRACE_OBJ_CREATE("finished");
 
